915a3072799943c6db3fa6f69d253cbcfe50e1dd
antoniojps <xixinhora@gmail.com>
2016-12-28 00:15:17 +0000

    Mapa.html feito com as arrays e tilemap

--- js/mapa.js
+++ js/mapa.js
@@ -1,4 +1,10 @@
-/* var numeroTile = 0;
+$(document).ready(function () {
+    detetaImg();
+    desenharMapa();
+
+});
+
+
 var arrayMapa = [
     [40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40],
     [40, 40, 40, 40, 60, 40, 40, 40, 40, 40, 40, 80, 40, 40, 100, 40, 40, 40, 40, 40, 40, 40, 40],
@@ -18,27 +24,75 @@ var arrayMapa = [
 ];
 
 function desenharMapa() {
+var tileNumber = 0;
     for (l = 0; l < arrayMapa.length; l++) {
-        for (c = 0; c < arrayMapa[c].length; c++) {
-            detetaImgMapa(l, c, arrayMapa[l][c]);
+
+        for (c = 0; c < arrayMapa[l].length; c++) {
+
+            var canvas = $('#canvas');
+            tileNumber++;
+
+            var idTileMap = $('#' + arrayMapa[l][c]);
+
+            var posX = parseInt(idTileMap.css('margin-left'));
+            var posY = parseInt(idTileMap.css('margin-top'));
+            canvas.append("<div id='tile" + tileNumber + "'></div>");
+            var tileId = $('#tile' + tileNumber);
+
+            tileId.css('margin-top', l * 48 + "px");
+            tileId.css('margin-left', c * 48 + "px");
+
+            tileId.css('height', '48px');
+            tileId.css('width', '48px');
+            tileId.css('background-image', 'url("img/mapa.png")');
+            tileId.css('background-position', (-posX) + "px " + (-posY) + "px");
+            tileId.css('position','absolute');
+
         }
     }
 }
 
-function detetaImgMapa(linha, coluna, tile) {
-    numeroTile++;
-    var canvas = $('#canvas');
-    canvas.append("<div id='" + numeroTile + "' ></div>");
 
-    var idTile = $('#' + numeroTile);
-    idTile.css('height','48px');
-    idTile.css('width','48px');
-    idTile.css('background-image', 'url("../img/mapa.png")');
+function detetaImg() {
+    var larguraTile = 20;
+    var alturaTile = 7;
+    var altura = 48;
+
+
+    for (i = 0; i <= (larguraTile * alturaTile) - 1; i++) {
+        $('#tileMap').append("<div class='absolute' id='" + i + "'>" + i + "</div>");
+        var id = $('#' + i);
+
+        if (i > 0 && i < 20) {
+            id.css('margin-top', 0);
+            id.css('margin-left', i * 48 + "px");
+        }
+        if (i >= 20 && i < 40) {
+            id.css('margin-top', altura + "px");
+            id.css('margin-left', (i - 20) * 48 + "px");
+        }
+        if (i >= 40 && i < 60) {
+            id.css('margin-top', altura * 2 + "px");
+            id.css('margin-left', (i - 40) * 48 + "px");
+        }
+        if (i >= 60 && i < 80) {
+            id.css('margin-top', altura * 3 + "px");
+            id.css('margin-left', (i - 60) * 48 + "px");
+        }
+        if (i >= 80 && i < 100) {
+            id.css('margin-top', altura * 4 + "px");
+            id.css('margin-left', (i - 80) * 48 + "px");
+        }
+        if (i >= 100 && i < 120) {
+            id.css('margin-top', altura * 5 + "px");
+            id.css('margin-left', (i - 100) * 48 + "px");
+        }
+        if (i >= 120 && i < 140) {
+            id.css('margin-top', altura * 6 + "px");
+            id.css('margin-left', (i - 120) * 48 + "px");
+        }
+    }
+
 }
 
-$(document).ready(function () {
-    desenharMapa();
-});
-
-*/
 
